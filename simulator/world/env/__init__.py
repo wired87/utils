682100fef05e3b1f1f4a3f0de73198d1dcbe1854
wired87 <derbenedikt.sterra@gmail.com>
2025-05-29 14:35:16 +0200

    mist

--- simulator/world/env/__init__.py
+++ simulator/world/env/__init__.py
@@ -1,11 +1,11 @@
-import os
-
 from bm.settings import OS_NAME
 from utils.file.yyaml import load_yaml
-
-ENV=r"C:\Users\wired\OneDrive\Desktop\Projects\Brainmaster\utils\simulator\world\env\data\uniform_env.yaml" if OS_NAME == "nt" else "utils/simulator/world/env/data/uniform_env.yaml"
-ENV_LEX=r"C:\Users\wired\OneDrive\Desktop\Projects\Brainmaster\utils\simulator\world\env\data\env_lex.yaml" if OS_NAME == "nt" else "utils/simulator/world/env/data/env_lex.yaml"
-
+if OS_NAME == "nt":
+    ENV=r"C:\Users\wired\OneDrive\Desktop\Projects\Brainmaster\utils\simulator\world\env\data\uniform_env.yaml"
+    ENV_LEX=r"C:\Users\wired\OneDrive\Desktop\Projects\Brainmaster\utils\simulator\world\env\data\env_lex.yaml"
+else:
+    ENV ="utils/simulator/world/env/data/uniform_env.yaml"
+    ENV_LEX = "utils/simulator/world/env/data/env_lex.yaml"
 ENV = load_yaml(ENV)
 ENV_LEX = load_yaml(ENV_LEX)
 
