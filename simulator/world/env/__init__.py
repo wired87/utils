d6f78cb6d0ce4698e779677526b8a4c14eea9bc3
wired87 <derbenedikt.sterra@gmail.com>
2025-05-29 14:42:57 +0200

    mist

--- simulator/world/env/__init__.py
+++ simulator/world/env/__init__.py
@@ -2,6 +2,7 @@ import os
 from utils.file.yyaml import load_yaml
 
 HERE = os.path.dirname(__file__)
+print("ENV", HERE)
 ENV = os.path.abspath(os.path.join(HERE, "data", "uniform_env.yaml"))
 ENV_LEX = os.path.abspath(os.path.join(HERE, "data", "env_lex.yaml"))
 
