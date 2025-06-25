13cb4513edbc2a9883450c1a03f784dbc71a596c
wired87 <derbenedikt.sterra@gmail.com>
2025-06-25 13:24:55 +0200

    psi

--- _np/serialize_complex.py
+++ _np/serialize_complex.py
@@ -1,2 +1,13 @@
-def serialize_complex(com):
-    return [[(z.real, z.imag) for z in row] for row in com]
+def serialize_complex(com, is_quark):
+    print("complex before s:", com)
+    if is_quark:
+        serialized_struct = []
+        for i in range(3):
+            row = com[i]  # com[i] ist eine 1D-Zeile (Spinor)
+            serialized_row = [(z.real, z.imag) for z in row]
+            serialized_struct.append(serialized_row)
+    else:
+        serialized_struct = [[(z.real, z.imag) for z in row] for row in com]
+
+    print("serialized_struct", serialized_struct)
+    return serialized_struct
